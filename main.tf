c4adf37624b933c222d205cf98ea5c873d6ba189
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-03-29 13:16:04 +0200

    feat: create Container Group

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+resource "azurerm_container_group" "this" {
+  name                = var.instance_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = var.os_type
 
+  ip_address_type = "None"
+
+  dynamic "container" {
+    for_each = var.containers
+
+    content {
+      name   = container.value["name"]
+      image  = container.value["image"]
+      cpu    = container.value["cpu"]
+      memory = container.value["memory"]
+    }
+  }
+
+  tags = var.tags
+}
