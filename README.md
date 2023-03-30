bfd95ff3cf1bc8e1e94e499791fc9836dc0323b1
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-03-30 07:26:04 +0200

    feat: create Container Group (#1)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Terraform Module Template
+# Azure Container Instances Terraform module
 
 [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
 [![Equinor Terraform Baseline](https://img.shields.io/badge/Equinor%20Terraform%20Baseline-1.0.0-blueviolet)](https://github.com/equinor/terraform-baseline)
 
-Template repository for Terraform modules.
+Terraform module which creates Azure Container Instances resources.
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "instance_name" {
+  description = "The name of this Container Instance."
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
@@ -6,6 +11,23 @@ variable "resource_group_name" {
 variable "location" {
   description = "The location to create the resources in."
   type        = string
+}
+
+variable "containers" {
+  description = "A list of containers to create for this Container Instance."
+
+  type = list(object({
+    name   = string
+    image  = string
+    cpu    = string
+    memory = string
+  }))
+}
+
+variable "os_type" {
+  description = "The OS type of this Container Instance."
+  type        = string
+  default     = "Linux"
 }
 
 variable "tags" {
