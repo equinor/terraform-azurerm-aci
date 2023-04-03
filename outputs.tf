1fbea4ea212e94a1266880676e12fdd874bad3ae
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-04-03 07:18:34 +0200

    feat: output IP address

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "identity_tenant_id" {
   description = "The tenant ID of the system-assigned identity of this Container Instance."
   value       = try(azurerm_container_group.this.identity[0].tenant_id, null)
 }
+
+output "ip_address" {
+  description = "The IP address of this Container Instance."
+  value       = azurerm_container_group.this.ip_address
+}
